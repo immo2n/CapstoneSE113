afd251963813f9cb867bededa41e8f521afeb2b9
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2024-02-28 11:04:35 +0600

    Update Class-28-2-2024.c

--- Class Practise/Class-28-2-2024.c
+++ Class Practise/Class-28-2-2024.c
@@ -1,3 +1,8 @@
+/*
+10 20 30
+50 60 70
+90 100 110
+*/
 #include <stdio.h>
 int main(){
     int arr[][3] = {
@@ -8,9 +13,11 @@ int main(){
 
     for(i = 0; i < sizeof(arr)/sizeof(arr[0]); ++i){
         for(j = 0; j < 3; ++j){
-            printf("%d\n", arr[i][j]);
+            printf("%d ", arr[i][j]);
         }
+        printf("\n");
     }
-
+    
     return 0;
 }
+
